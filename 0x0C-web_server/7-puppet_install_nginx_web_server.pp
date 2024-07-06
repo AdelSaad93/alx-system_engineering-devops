6bd9aa1383aea8219f14de9c2d577d14964ea133
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-07-05 21:40:50 -0700

    var

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,16 +4,51 @@ package { 'nginx':
   ensure => 'installed',
 }
 
-file { 'index.nginx-debian.html':
-  path    => '/var/www/html/index.nginx-debian.html',
+file { '/var/www/html/index.nginx-debian.html':
   content => 'Hello World!',
 }
 
-exec { 'config':
-  command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com/watch?v=TfgBHC5gvTI permanent;/" /etc/nginx/sites-available/default',
-  provider => 'shell',
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => template('nginx/default.erb'),
+  notify  => Exec['restart_nginx'],
 }
-exec { 'start':
-  command  => 'sudo service nginx restart',
-  provider => 'shell',
+
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default',
+}
+
+exec { 'restart_nginx':
+  command     => 'service nginx restart',
+  refreshonly => true,
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure => 'present',
+  content => template('nginx/default.erb'),
+}
+
+# Template file to set up the site configuration
+file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
+  ensure  => 'file',
+  content => @("END")
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /redirect_me {
+        rewrite ^/redirect_me https://www.youtube.com/watch?v=TfgBHC5gvTI permanent;
+    }
+}
+| END
 }
