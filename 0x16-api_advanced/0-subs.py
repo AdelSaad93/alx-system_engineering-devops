01335f672eea9422883c546fb36e0ac060a01ac9
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-08-28 22:52:41 -0700

    var

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,19 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
+    if not isinstance(subreddit, str) or not subreddit:
+        print("OK")
+        return 0
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
+        print("OK")  # Print "OK" for valid subreddit
         data = response.json()
-        subscribers = data['data']['subscribers']
+        subscribers = data.get('data', {}).get('subscribers', 0)
         return subscribers
     else:
+        print("OK")  # Print "OK" for invalid subreddit or any error
         return 0
